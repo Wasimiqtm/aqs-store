35e03b7f03ec5b4763df1a29e83f343bdc525d83
wasim939 <wasim.iqtm@gmail.com>
2023-12-07 19:36:18 +0500

    add title and meta description

--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('title')
+    {!! $page->title !!}
+@endsection
+
+@section('description')
+    {!! $page->description !!}
+@endsection
+
 @section('content')
 <section class="flat-map">
     <div class="container">
--- resources/views/pages/pages_view.blade.php
+++ resources/views/pages/pages_view.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('title')
+    {!! $page->title !!}
+@endsection
+
+@section('description')
+    {!! $page->description !!}
+@endsection
+
 @section('content')
 
 {!! $page->content !!}
